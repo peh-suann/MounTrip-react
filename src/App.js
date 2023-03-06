5c824b8c45ec651893893507cd22b6871e8a15ce
carol <lu773414@gmail.com>
2023-03-06 15:37:05 +0800

    巢狀調整

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/Mountrip.module.css'
 
 import Layout from './layouts/Layout'
-import TestMemberList from './pages/TestMemberList'
 import Login from './pages/KexinLogin'
 import YichunProducts from './pages/YichunProducts'
 
