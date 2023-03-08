62f2b58e78527899635923f038a03a4b007df750
kexin lu <lu773414@gmail.com>
2023-03-08 18:15:37 +0800

    map動畫、reset

--- src/pages/KexinIndex.js
+++ src/pages/KexinIndex.js
@@ -1,6 +1,6 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react'
 import * as d3 from 'd3'
-// import NavbarIndex from '../layouts/NavbarIndex'
+import NavbarIndex from '../layouts/NavbarIndex'
 import IndexStyles from '../styles/kexinIndex.module.css'
 
 function KexinIndex() {
@@ -9,7 +9,7 @@ function KexinIndex() {
   // map basic setting
   const WIDTH = window.innerWidth
   const HEIGHT = window.innerHeight
-  const ZOOM_THRESHOLD = [0.3, 10]
+  const ZOOM_THRESHOLD = [1, 5]
   const OVERLAY_MULTIPLIER = 10
   const OVERLAY_OFFSET = OVERLAY_MULTIPLIER / 2 - 0.5
   const ZOOM_DURATION = 500
@@ -18,64 +18,48 @@ function KexinIndex() {
   const HOVER_COLOR = 'rgba(10, 140, 45, 0.4)'
   const CLICK_COLOR = 'rgba(10, 140, 45, 0.6)'
 
-  var data = require('../mapdata/tw_new.json'); 
-  console.log(data);
+  var data = require('../mapdata/tw_new.json')
 
-  // const [data, setData] = useState([]);
-  // useEffect(() => {
-  //   // var data = require('../mapdata/tw_new.json'); 
-  //   // console.log(data);
-  //   setData(data)
-  // }, []);
-
-  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  const mapRef = useRef(null);
-  const [selectedFeature, setSelectedFeature] = useState(null);
+  const mapRef = useRef(null)
+  const [selectedFeature, setSelectedFeature] = useState(null)
 
   useEffect(() => {
     if (data) {
-      
-
-      const zoom = d3
-        .zoom()
-        .scaleExtent(ZOOM_THRESHOLD)
-        .on("zoom", zoomHandler);
+      const zoom = d3.zoom().scaleExtent(ZOOM_THRESHOLD).on('zoom', zoomHandler)
 
       function zoomHandler() {
-        g.attr("transform", d3.zoomTransform(this));
-        console.log(d3.zoomTransform(this));
+        g.attr('transform', d3.zoomTransform(this))
+        console.log(d3.zoomTransform(this))
       }
 
       const svg = d3
-      .select(mapRef.current)
-        // .select("#info")
-        .append("svg")
-        .attr("width", "100%")
-        .attr("height", "100%");
+        .select(mapRef.current)
+        .append('svg')
+        .attr('id', 'map')
+        .attr('width', '100%')
+        .attr('height', '100%')
 
-      const g = svg.call(zoom).append("g");
+      const g = svg.call(zoom).append('g').attr('id', 'mapZoom')
 
-      g
-        .append("rect")
-        .attr("width", WIDTH * OVERLAY_MULTIPLIER)
-        .attr("height", HEIGHT * OVERLAY_MULTIPLIER)
+      g.append('rect')
+        .attr('width', WIDTH * OVERLAY_MULTIPLIER)
+        .attr('height', HEIGHT * OVERLAY_MULTIPLIER)
         .attr(
-          "transform",
+          'transform',
           `translate(-${WIDTH * OVERLAY_OFFSET},-${HEIGHT * OVERLAY_OFFSET})`
         )
-        .style("fill", "none")
-        .style("pointer-events", "all");
+        .style('fill', 'none')
+        .style('pointer-events', 'all')
 
       // Create a new projection
       const projection = d3
-      .geoMercator()
-      .center([121, 23.58])
-      .scale(10000)
-      .translate([WIDTH / 2, HEIGHT / 2]);
+        .geoMercator()
+        .center([121, 23.58])
+        .scale(10000)
+        .translate([WIDTH / 2, HEIGHT / 2])
 
       // Create a path generator
-      const path = d3.geoPath(projection);
+      const path = d3.geoPath(projection)
 
       // Draw the map
       g.selectAll('path')
@@ -83,49 +67,81 @@ function KexinIndex() {
         .enter()
         .append('path')
         .attr('d', path)
-        .style('fill', "rgba(10, 140, 45, 0.2)")
-        .style('stroke',"#FFFFF2")
+        .style('fill', 'rgba(10, 140, 45, 0.2)')
+        // .style('fill', function (d) {
+        //   return d.properties.fill // replace with your own data value accessor
+        // })
+        .style('stroke', '#FFFFF2')
         .style('stroke-width', 0.5)
-        .style("cursor","pointer")
-        .on("mouseover", mouseOverHandler)
-        .on("mouseout", mouseOutHandler)
-        .on("wheel.zoom",null)
-        .on("click", clickHandler);
+        .style('cursor', 'pointer')
+        .on('mouseover', mouseOverHandler)
+        .on('mouseout', mouseOutHandler)
+        .on('click', clickHandler)
 
-      function mouseOverHandler(d, i) {
-        // setSvgElement(d)
-        if (d3.select(this).attr("fill")!=CLICK_COLOR) {
-          d3.select(this).attr("fill", HOVER_COLOR)
-        }
-      }
+      // d3.select("svg").on("dblclick.zoom", null);
 
-      function mouseOutHandler(d, i) {
-        if (d3.select(this).attr("fill")!=CLICK_COLOR) {
-          d3.select(this).attr("fill", "rgba(10, 140, 45, 0.2)")
-        }
-      }
+      d3.select(mapRef.current).on('click', clickReset)
 
-      function clickHandler(d, i) {
-        console.log(this);
-        d3.selectAll("path").attr("fill", "rgba(10, 140, 45, 0.2)")
-        // console.log(d)
-        d3.select(this).attr("fill", CLICK_COLOR)
-        svg.transition()
-           .duration(ZOOM_DURATION)
-           .call(
-        zoom.transform,
-        d3.zoomIdentity.scale(1).translate(-130,-190)
-        );
-      }
-
-     
+      // function clickReset(e) {
+      //   if (e.target.style.fill == 'none') {
+      //     console.log('reset')
+      //     svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
+      //     d3.select(mapRef.current).selectAll("path").style("fill", "rgba(10, 140, 45, 0.2)");
+      //   }
+      //   // d3.select(mapRef.current)
+      // }
     }
-  }, []);
+
+  }, [])
+
+  const mouseOverHandler = function (d, i) {
+    if (d3.select(this).style('fill') != CLICK_COLOR) {
+      d3.select(this).style('fill', HOVER_COLOR)
+    }
+  }
+
+  const mouseOutHandler = function (d, i) {
+    if (d3.select(this).style('fill') != CLICK_COLOR) {
+      d3.select(this).style('fill', 'rgba(10, 140, 45, 0.2)')
+    }
+  }
+
+  const clickHandler = function (d, i, e) {
+    d3.select(mapRef.current)
+      .selectAll('path')
+      .style('fill', 'rgba(10, 140, 45, 0.2)')
+    // // console.log(d)
+    d3.select(this).style('fill', CLICK_COLOR)
+    // svg
+    //   .transition()
+    //   .duration(ZOOM_DURATION)
+    //   .call(zoom.transform, d3.zoomIdentity.scale(1).translate(-130, -190))
+
+    console.log(d3.select(this).style('fill'))
+
+    // if(d3.select(this).style.fill=='none'){
+
+    // }
+  }
+
+  const clickReset = function(e) {
+    console.log(window.innerWidth, window.innerHeight)
+    if(e.target.style.fill=='none'){
+      console.log("hiclick")
+      console.log(d3.select('#mapZoom'))
+      d3.select('#mapZoom')
+        .transition()
+        .duration(750)
+        .attr('transform','translate(0,0) scale(1)')
+
+      d3.select(mapRef.current).selectAll("path").style("fill", "rgba(10, 140, 45, 0.2)");
+    }
+  }
 
   return (
     <>
-      {/* <NavbarIndex /> */}
-      <div ref={mapRef} id={`${IndexStyles['info']}`}></div>
+      <NavbarIndex />
+      <div ref={mapRef} id={`${IndexStyles['info']}`} onClick={clickReset}></div>
     </>
   )
 }
