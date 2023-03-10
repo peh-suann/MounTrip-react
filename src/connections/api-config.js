7c8128d71032cabdc5035a307e04f835a88a2d14
Davispro <davispromail@gmail.com>
2023-03-10 12:17:02 +0800

    commit

--- src/connections/api-config.js
+++ src/connections/api-config.js
@@ -7,6 +7,8 @@ export const LIST_DATA = `${HOST}/member`
 
 export const TRAILS_DATA = `${HOST}/trails`
 
+export const TRAILS_BATCH_DATA = `${HOST}/trails-batch`
+
 //教室電腦的連線
 // export const HOST = '192.168.21.84:3000'
 
