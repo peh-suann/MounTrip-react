c1e6a44511177db9851fe5fa311585ad2a2c71bf
Davispro <davispromail@gmail.com>
2023-03-19 23:44:52 +0800

    commit

--- src/components/DavisTrailsFilter/DavisComFilterCardFilter.js
+++ src/components/DavisTrailsFilter/DavisComFilterCardFilter.js
@@ -16,8 +16,6 @@ function DavisComFilterCardFilter(props) {
     rows: [],
   })
 
-  let rows_data = alldata.rows
-
   const getListData = async (page = 1) => {
     const response = await axios.get(FILTER_ALL_DATA, {
       params: {
@@ -28,6 +26,8 @@ function DavisComFilterCardFilter(props) {
     setAlldata(response.data)
   }
 
+  let rows_data = alldata.rows
+
   // const [pagedata, setpagedata] = useState({ rows: [] })
 
   // 分頁
@@ -37,17 +37,12 @@ function DavisComFilterCardFilter(props) {
   // const endIndex = startIndex + perPage
 
   // TODO:收藏按鈕
-  const initState = rows_data.map((v, i) => {
-    return { ...v, collect: false }
-  })
 
-  const [collect, setCollect] = useState(initState)
+  const [collect, setCollect] = useState([])
 
-  console.log(initState)
-
-  const toggleCollect = (arr, id) => {
-    return arr.map((v, i) => {
-      if (v.trails_sid === id) return { ...v, collect: !v.collect }
+  const toggleCollect = (rows_data, trails_sid) => {
+    return rows_data.map((v, i) => {
+      if (v.trails_sid === trails_sid) return { ...v, collect: !v.collect }
       else return { ...v }
     })
   }
@@ -80,6 +75,12 @@ function DavisComFilterCardFilter(props) {
     }
   }, [])
 
+  useEffect(() => {
+    const modifiedData = alldata.rows.map((v) => ({ ...v, collect: false }))
+    setCollect(modifiedData)
+    console.log(modifiedData)
+  }, [alldata])
+
   // useEffect(() => {
   //   console.log('useEffect--')
   //   setAlldata(alldata)
@@ -95,7 +96,7 @@ function DavisComFilterCardFilter(props) {
       {/* {data.rows.map((r) => ( */}
       {oddRows(
         filterByKeyword(
-          alldata.rows,
+          rows_data,
           keywordpr,
           startdatepr,
           enddatepr,
@@ -109,7 +110,7 @@ function DavisComFilterCardFilter(props) {
             <div className="row g-0 d-flex flex-row ">
               <div className={`col-4 ${styles.trails_img_wrap}`}>
                 <img
-                  src={`/images/public_images/product_image/${r.sid}-1.jpg`}
+                  src={`/images/public_images/product_image/${r.sid}-2.jpg`}
                   className={`rounded-start ${styles.trails_img}`}
                   alt="..."
                 />
@@ -236,48 +237,45 @@ function DavisComFilterCardFilter(props) {
               <div className="col-1  d-flex justify-content-end align-items-start">
                 <button
                   onClick={() => {
-                    setCollect(toggleCollect(collect, r.trail_sid))
+                    setCollect(toggleCollect(rows_data, r.trails_sid))
+                    console.log(rows_data, r.trails_sid)
                   }}
                   className={`${styles.heart_btn}`}
                 >
-                  {/* normal heart */}
-                  <svg
-                    className={
-                      `${r.collect}`
-                        ? `${styles.collect_heart}`
-                        : `${styles.not_collect}`
-                    }
-                    width="50"
-                    height="75"
-                    viewBox="0 0 50 75"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M20.3882 29.6118C19.8775 29.1008 19.271 28.6955 18.6036 28.4189C17.9361 28.1423 17.2207 28 16.4982 28C15.7757 28 15.0603 28.1423 14.3929 28.4189C13.7254 28.6955 13.119 29.1008 12.6082 29.6118L11.5482 30.6718L10.4882 29.6118C9.45652 28.5801 8.05725 28.0005 6.59821 28.0005C5.13918 28.0005 3.73991 28.5801 2.70821 29.6118C1.67652 30.6435 1.09692 32.0428 1.09692 33.5018C1.09692 34.9609 1.67652 36.3601 2.70821 37.3918L3.76821 38.4518L11.5482 46.2318L19.3282 38.4518L20.3882 37.3918C20.8992 36.8811 21.3046 36.2746 21.5811 35.6072C21.8577 34.9397 22 34.2243 22 33.5018C22 32.7793 21.8577 32.0639 21.5811 31.3965C21.3046 30.729 20.8992 30.1226 20.3882 29.6118Z"
-                      stroke="#6CBA7C"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  {/* red heart */}
-                  <svg
-                    width="23"
-                    height="21"
-                    viewBox="0 0 23 21"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M20.3882 2.61183C19.8775 2.10083 19.271 1.69547 18.6036 1.41891C17.9361 1.14235 17.2207 1 16.4982 1C15.7757 1 15.0603 1.14235 14.3929 1.41891C13.7254 1.69547 13.119 2.10083 12.6082 2.61183L11.5482 3.67183L10.4882 2.61183C9.45652 1.58013 8.05725 1.00053 6.59821 1.00053C5.13918 1.00053 3.73991 1.58013 2.70821 2.61183C1.67652 3.64352 1.09692 5.04279 1.09692 6.50183C1.09692 7.96086 1.67652 9.36013 2.70821 10.3918L3.76821 11.4518L11.5482 19.2318L19.3282 11.4518L20.3882 10.3918C20.8992 9.88107 21.3046 9.27464 21.5811 8.60718C21.8577 7.93972 22 7.22431 22 6.50183C22 5.77934 21.8577 5.06393 21.5811 4.39647C21.3046 3.72901 20.8992 3.12258 20.3882 2.61183Z"
-                      fill="#FF0000"
-                      stroke="#6CBA7C"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
+                  {r.collect ? (
+                    <svg
+                      width="50"
+                      height="75"
+                      viewBox="0 0 50 75"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M20.3882 29.6118C19.8775 29.1008 19.271 28.6955 18.6036 28.4189C17.9361 28.1423 17.2207 28 16.4982 28C15.7757 28 15.0603 28.1423 14.3929 28.4189C13.7254 28.6955 13.119 29.1008 12.6082 29.6118L11.5482 30.6718L10.4882 29.6118C9.45652 28.5801 8.05725 28.0005 6.59821 28.0005C5.13918 28.0005 3.73991 28.5801 2.70821 29.6118C1.67652 30.6435 1.09692 32.0428 1.09692 33.5018C1.09692 34.9609 1.67652 36.3601 2.70821 37.3918L3.76821 38.4518L11.5482 46.2318L19.3282 38.4518L20.3882 37.3918C20.8992 36.8811 21.3046 36.2746 21.5811 35.6072C21.8577 34.9397 22 34.2243 22 33.5018C22 32.7793 21.8577 32.0639 21.5811 31.3965C21.3046 30.729 20.8992 30.1226 20.3882 29.6118Z"
+                        stroke="#6CBA7C"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      width="50"
+                      height="75"
+                      viewBox="0 0 50 75"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M20.3882 29.6118C19.8775 29.1008 19.271 28.6955 18.6036 28.4189C17.9361 28.1423 17.2207 28 16.4982 28C15.7757 28 15.0603 28.1423 14.3929 28.4189C13.7254 28.6955 13.119 29.1008 12.6082 29.6118L11.5482 30.6718L10.4882 29.6118C9.45652 28.5801 8.05725 28.0005 6.59821 28.0005C5.13918 28.0005 3.73991 28.5801 2.70821 29.6118C1.67652 30.6435 1.09692 32.0428 1.09692 33.5018C1.09692 34.9609 1.67652 36.3601 2.70821 37.3918L3.76821 38.4518L11.5482 46.2318L19.3282 38.4518L20.3882 37.3918C20.8992 36.8811 21.3046 36.2746 21.5811 35.6072C21.8577 34.9397 22 34.2243 22 33.5018C22 32.7793 21.8577 32.0639 21.5811 31.3965C21.3046 30.729 20.8992 30.1226 20.3882 29.6118Z"
+                        fill="#FF0000"
+                        stroke="#6CBA7C"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  )}
                 </button>
               </div>
             </div>
