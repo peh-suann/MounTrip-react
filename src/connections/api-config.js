4ab93879f885b6d426367d8f5b999bebabe7a086
yichun <yichunccc0830@gmail.com>
2023-03-21 12:43:55 +0800

    fix connection

--- src/connections/api-config.js
+++ src/connections/api-config.js
@@ -4,11 +4,11 @@
 // hosts ---------------------------------------------------------------------------
 // export const HOST = 'http://localhost:3001'
 // export const HOST = 'http://localhost:3002'
-export const HOST = 'http://localhost:3001'
+// export const HOST = 'http://localhost:3001'
 
 //Lai's connect
 // export const HOST = 'http://192.168.71.105:3001'
-export const HOST = 'http://127.0.0.1:3002'
+// export const HOST = 'http://127.0.0.1:3002'
 
 // 兩台電腦連線時，要用筆電wiki的ip，才能使用localstorage
 
@@ -18,7 +18,7 @@ export const HOST = 'http://127.0.0.1:3002'
 // export const HOST = '192.168.21.84:3002'
 
 // yichun's connect
-// export const HOST = 'http://192.168.71.118:3000'
+export const HOST = 'http://192.168.71.118:3001'
 
 // export const HOST = 'http://192.168.71.105:3001'
 
